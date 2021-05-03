cea18b20a8b8579b36ebc2a383a375031fadcbdb
Vu Thang <dangvuthang97@gmail.com>
2021-05-03 23:59:38 +0700

    Add Item class

---
+++ assignment2_group5/Item.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+class Item {
+	private:
+		string id;
+		string title;
+		string rentalType;
+		string loanType;
+		int numberOfCopies;
+		float rentalFee;
+		string rentalStatus;
+	public:
+		Item(string id, string title, string rentalType, string loanType, int numberOfCopies, float rentalFee, string rentalStatus);
+		Item();
+		string GetId();
+		string GetTitle();
+		string GetRentalType();
+		string GetLoanType();
+		int GetNumberOfCopies();
+		float GetRentalFee();
+		string GetRentalStatus();
+		void SetId();
+		void SetTitle();
+		void SetRentalType(int type);
+		void SetLoanType();
+		void SetNumberOfCopies();
+		void SetRentalFee();
+		void SetRentalStatus();
+		string ToString();
+		static bool CheckId(string input);
+};
+
